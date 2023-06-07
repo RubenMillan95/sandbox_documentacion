78bf81c4552827070437e57910548547c0ace20a
RubenMillan95 <necroptich@gmail.com>
2023-06-07 12:17:54 -0500

    version 0.0.2.3

--- src/App.ts
+++ src/App.ts
@@ -2,7 +2,7 @@ import swaggerUI from 'swagger-ui-express'
 import { swaggerSpec } from './swagger.conf'
 
 import express,{Application, Request, Response }from 'express'
-import {PrismaClient} from '@prisma/client'
+import PacienteRouter from  './routes/PacienteRouter'
 
 
 /**
@@ -16,7 +16,7 @@ class App{
 	//Atributos
 	public app:any
 	private server:any
-	private prismaClient:PrismaClient
+	
 
 	constructor(){
 		this.app=express()
@@ -26,7 +26,7 @@ class App{
 			swaggerUI.serve,
 			swaggerUI.setup(swaggerSpec)
 		)
-        this.prismaClient=new PrismaClient()  
+          
 
 		this.routes()
 	}
@@ -35,55 +35,7 @@ class App{
 	 * Definir y agregar las rutas de la API con express
 	 */
 	private routes():void{
-		this.app.get(
-			'/',
-			(req:Request, res:Response)=>{
-				res.send('Bienvenidos a typescript')
-			}
-		) 
-
-		this.app.get(
-			'/pacientes',async (req:Request, res:Response)=>{
-			const pacientes=await this.prismaClient.paciente.findMany() 
-				res.json(pacientes)
-			}
-		)
-
-
-		this.app.post(
-			'/crear_paciente',
-			async (req:Request, res:Response)=>{
-				try{
-
-				const{
-					cedula,
-					nombre,
-					apellido,
-					fecha,
-					telefono
-				}=req.body
-
-				const fechaNacimiento= new Date(fecha)
-
-				const paciente=await this.prismaClient.paciente.create(
-					{
-						data:{cedula,
-							nombre,
-							apellido,
-							fechaNacimiento,
-							telefono}
-					}
-				)
-
-
-
-				res.json(req.body)
-			}catch(e:any){
-				res.status(400)
-				res.json({error:e.message})
-			}
-			}
-		)
+		this.app.use('/', PacienteRouter)
 	}
 	public start():void{
 		this.server=this.app.listen(
